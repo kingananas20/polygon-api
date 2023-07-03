54428baaed83473ec4bbe3dedc5795a048a01b95
kingananas20 <jonasarter@outlook.com>
2023-07-03 18:21:24 +0200

    Added cosmetics.json file

--- scr/index.js
+++ scr/index.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const weapons = JSON.parse(fs.readFileSync("data/weapons.json"));
 const modules = JSON.parse(fs.readFileSync("data/modules.json"));
+const cosmetics = JSON.parse(fs.readFileSync("data/cosmetics.json"));
 
 const app = express();
 
@@ -26,7 +27,7 @@ app.get("/api/v1/weapons/:code", (req, res) => {
   res.send(weapon);
 });
 
-app.get("/api/v1/modules/optics/:code", (reg, res) => {
+app.get("/api/v1/modules/optics/:code", (req, res) => {
   const query = req.params.code.toUpperCase();
   const module = modules.data.optic.find((c) => c.code === query);
 
