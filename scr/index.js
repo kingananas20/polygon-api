aebcd24c768fe708feb26a289d6a7388136d1776
kingananas20 <jonasarter@outlook.com>
2023-07-03 18:09:56 +0200

    Added codes to modules and made it possible to get the data of the optic modules

--- scr/index.js
+++ scr/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 require("dotenv").config();
 
 const weapons = JSON.parse(fs.readFileSync("data/weapons.json"));
+const modules = JSON.parse(fs.readFileSync("data/modules.json"));
 
 const app = express();
 
@@ -14,13 +15,26 @@ app.get("/api/weapons", (req, res) => {
   res.send(weapons);
 });
 
-app.get("/api/weapons/:code", (req, res) => {
+app.get("/api/v1/weapons/:code", (req, res) => {
   const query = req.params.code.toUpperCase();
   const weapon = weapons.data.find((c) => c.code === query);
+
   if (!weapon) {
     res.status(404).send("That weapon does not exist!");
   }
+
   res.send(weapon);
+});
+
+app.get("/api/v1/modules/optics/:code", (reg, res) => {
+  const query = req.params.code.toUpperCase();
+  const module = modules.data.optic.find((c) => c.code === query);
+
+  if (!module) {
+    res.status(404).send("That weapon does not exist!");
+  }
+
+  res.send(module);
 });
 
 const port = process.env.PORT || 3000;
