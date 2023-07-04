81a468b1609ac6333d396f64f30cdfecb3bddff4
kingananas20 <jonasarter@outlook.com>
2023-07-04 20:12:45 +0200

    Added a different url syntax for modules and made the apikey limit to work

--- scr/index.js
+++ scr/index.js
@@ -10,18 +10,18 @@ const keys = JSON.parse(fs.readFileSync("data/keys.json"));
 const app = express();
 
 function getPermission(key, res, payload) {
-  const permissionGranted = keys.keys.find((c) => c.key === key);
+  const permissionGranted = keys.find((c) => c.key === key);
   if (!permissionGranted) {
     res.send("incorrect");
+    return;
   } else if (permissionGranted.limit === 0) {
-    res.send("Max calls for today reached");
+    res.send("Max calls reached for today");
+    return;
   } else {
-    res.send(payload);
     permissionGranted.limit = permissionGranted.limit - 1;
-    fs.writeFileSync(
-      "data/keys.json",
-      JSON.stringify(permissionGranted, null, 2)
-    );
+    fs.writeFileSync("data/keys.json", JSON.stringify(keys, null, 2));
+    res.send(payload);
+    return;
   }
 }
 
@@ -29,34 +29,69 @@ app.get("/api/v1/:key", (req, res) => {
   getPermission(req.params.key, res, "correct");
 });
 
-app.get("/api/v1/weapons/", (req, res) => {
-  res.send(weapons);
+app.get("/api/v1/:key/weapons/", (req, res) => {
+  getPermission(req.params.key, res, weapons);
 });
 
-app.get("/api/:apikey/v1/weapons/:code", (req, res) => {
+app.get("/api/v1/:key/weapons/:code", (req, res) => {
   const query = req.params.code.toUpperCase();
   const weapon = weapons.data.find((c) => c.code === query);
 
   if (!weapon) {
     res.status(404).send("That weapon does not exist!");
+    return;
   }
 
-  res.send(weapon);
+  getPermission(req.params.key, res, weapon);
 });
 
-app.get("/api/v1/modules/optics/:code", (req, res) => {
-  const query = req.params.code.toUpperCase();
-  const module = modules.data.optic.find((c) => c.code === query);
+app.get("/api/v1/:key/modules", (req, res) => {
+  getPermission(req.params.key, res, modules);
+});
 
-  if (!module) {
-    res.status(404).send({
-      data: [],
-      message: "Query does not exist.",
-      success: 0,
-    });
+app.get("/api/v1/:key/modules/:type", (req, res) => {
+  if (req.params.type === "optics") {
+    getPermission(req.params.key, res, modules.data.optic);
+    return;
+  } else if (req.params.type === "barrels") {
+    getPermission(req.params.key, res, modules.data.barrel);
+    return;
+  } else if (req.params.type === "underbarrels") {
+    getPermission(req.params.key, res, modules.data.underbarrel);
+    return;
+  } else if (req.params.type === "accessory") {
+    getPermission(req.params.key, res, modules.data.accessory);
+    return;
+  } else {
+    res.send("Type of modules does not exist.");
+    return;
   }
+});
 
-  res.send(module);
+app.get("/api/v1/:key/modules/:type/:code", (req, res) => {
+  if (req.params.type === "optics") {
+    const module = modules.data.optic.find(
+      (c) => c.code === req.params.code.toUpperCase()
+    );
+    if (!module) {
+      res.send("That module does not exist.");
+      return;
+    }
+    getPermission(req.params.key, res, module);
+    return;
+  } else if (req.params.type === "barrels") {
+    getPermission(req.params.key, res, modules.data.barrel);
+    return;
+  } else if (req.params.type === "underbarrels") {
+    getPermission(req.params.key, res, modules.data.underbarrel);
+    return;
+  } else if (req.params.type === "accessory") {
+    getPermission(req.params.key, res, modules.data.accessory);
+    return;
+  } else {
+    res.send("Type of module does not exist");
+    return;
+  }
 });
 
 const port = process.env.PORT || 3000;
