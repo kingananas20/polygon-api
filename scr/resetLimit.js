b64e6d5d49daac44c14b573903dad60656f32312
kingananas20 <jonasarter@outlook.com>
2023-07-05 17:10:49 +0200

    Limit now resets on 12pm everyday

--- scr/resetLimit.js
+++ scr/resetLimit.js
@@ -1,15 +1,16 @@
 const schedule = require("node-schedule");
 const fs = require("fs");
 
-const keys = JSON.parse(fs.writeFileSync("data/keys.json"));
+const keys = JSON.parse(fs.readFileSync("data/keys.json"));
 
 function resetLimit() {
   for (i = 0; i < keys.length; i++) {
-    keys[i].limit = 1000;
+    const limit = keys[i];
+    limit.limit = 1000;
     fs.writeFileSync("data/keys.json", JSON.stringify(keys, null, 2));
   }
 }
 
-schedule.scheduleJob("*/5 * * * * *", function () {
+schedule.scheduleJob("0 12 * * *", function () {
   resetLimit();
 });
