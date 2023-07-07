ff4acd512bc927d2d6466dbc60f9791190d1e988
kingananas20 <jonasarter@outlook.com>
2023-07-07 20:49:14 +0200

    Updated some stuff

--- scr/index.js
+++ scr/index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const { createHash } = require("crypto");
 const express = require("express");
+const { get } = require("http");
 require("dotenv").config();
 
 const weapons = JSON.parse(fs.readFileSync("data/weapons.json"));
@@ -14,10 +15,10 @@ function hash(input) {
   return createHash("sha256").update(input).digest("hex");
 }
 
-function getPermission(key, res, payload) {
+function getPermission(key, res, payload, status) {
   const permissionGranted = keys.find((c) => c.key === hash(key));
   if (!permissionGranted) {
-    res.send("incorrect");
+    res.send("Apikey invalid or incorrect.").status(401);
     return;
   } else if (permissionGranted.limit === 0) {
     res.send("Max calls reached for today");
@@ -25,7 +26,7 @@ function getPermission(key, res, payload) {
   } else {
     permissionGranted.limit = permissionGranted.limit - 1;
     fs.writeFileSync("data/keys.json", JSON.stringify(keys, null, 2));
-    res.send(payload);
+    res.send(payload).status(status || 200);
     return;
   }
 }
@@ -79,28 +80,94 @@ app.get("/api/v1/:key/modules/:type/:code", (req, res) => {
       (c) => c.code === req.params.code.toUpperCase()
     );
     if (!module) {
-      res.send("That module does not exist.");
+      getPermission(req.params.key, res, "Optic does not exist.", 404);
       return;
     }
     getPermission(req.params.key, res, module);
     return;
   } else if (req.params.type === "barrels") {
+    const module = modules.data.barrel.find((c) => c.code === req.params.code);
+    if (!module) {
+      getPermission(req.params.key, res, "Barrel does not exist.", 404);
+      return;
+    }
     getPermission(req.params.key, res, modules.data.barrel);
     return;
   } else if (req.params.type === "underbarrels") {
-    getPermission(req.params.key, res, modules.data.underbarrel);
+    const module = modules.data.underbarrel.find(
+      (c) => c.code === req.params.code
+    );
+    if (!module) {
+      getPermission(req.params.key, res, "Underbarrel does not exist.", 404);
+      return;
+    }
+    getPermission(req.params.key, res, module);
     return;
   } else if (req.params.type === "accessory") {
     const module = modules.data.accessory.find(
       (c) => c.code === req.params.code.toUpperCase()
     );
     if (!module) {
-      res.send("That module does not exist.");
+      getPermission(req.params.key, res, "Accessory does not exist.", 404);
     }
     getPermission(req.params.key, res, module);
     return;
   } else {
-    res.send("Type of module does not exist");
+    getPermission(
+      req.params.key,
+      res,
+      "Incorrect type of modules {optics | barrels | underbarrels | accessory}.",
+      404
+    );
+    return;
+  }
+});
+
+app.get("/api/v1/:key/cosmetics", (req, res) => {
+  getPermission(req.params.key, res, cosmetics);
+  return;
+});
+
+app.get("/api/v1/:key/cosmetics/:type", (req, res) => {
+  if (req.params.type === "skins") {
+    getPermission(req.params.key, res, cosmetics.data.skins);
+    return;
+  } else if (req.params.type === "straps") {
+    getPermission(req.params.key, res, cosmetics.data.straps);
+    return;
+  } else {
+    getPermission(
+      req.params.key,
+      res,
+      "Incorrect type of cosmetics {skins | straps}.",
+      404
+    );
+    return;
+  }
+});
+
+app.get("/api/v1/:key/cosmetics/:type/:code", (req, res) => {
+  if (req.params.type === "skins") {
+    const cosmetic = cosmetics.data.skins.find(
+      (c) => c.code === req.params.code
+    );
+    if (!cosmetic) {
+    }
+    getPermission(req.params.key, res, cosmetic);
+    return;
+  } else if (req.params.type === "straps") {
+    const cosmetic = cosmetics.data.straps.find(
+      (c) => c.code === req.params.code
+    );
+    getPermission(req.params.key, res, cosmetic);
+    return;
+  } else {
+    getPermission(
+      req.params.key,
+      res,
+      "Incorrect type of cosmetics {skins | straps}.",
+      404
+    );
     return;
   }
 });
